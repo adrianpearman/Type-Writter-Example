aa8cc95f92342c76b6390b6f1858a35e98b89614
Adrian Pearman <adrianpearman12@gmail.com>
2018-10-04 02:38:29 -0400

    additonal styling added to the type writter, converting the ES5 Constructor to an ES6 Class

--- app.js
+++ app.js
@@ -1,55 +1,114 @@
-// ES5 Methodz
-const TypeWriter = function(txtElement, words, wait = 3000){
-  this.txtElement = txtElement
-  this.words = words
-  this.txt = ''
-  this.wordIndex = 0
-  this.wait = parseInt(wait, 10)
-  this.type()
-  this.isDeleting = false
-}
+// ------ ES5 Constructor Method ------ 
+// const TypeWriter = function(txtElement, words, wait = 3000){
+//   this.txtElement = txtElement
+//   this.words = words
+//   this.txt = ''
+//   this.wordIndex = 0
+//   this.wait = parseInt(wait, 10)
+//   this.type()
+//   this.isDeleting = false
+// }
 
-// Type Method
-TypeWriter.prototype.type = function(){
-  // Current Index of word
-  const current = this.wordIndex % this.words.length
-  // Get full text of current word
-  const fullTXT = this.words[current]
 
-  // Check if word is being deleted
-  if(this.isDeleting){
-    // Remove a character
-    this.txt = fullTXT.substring(0, this.txt.length - 1)
-  }else{
-    // Adding a character
-    this.txt = fullTXT.substring(0, this.txt.length + 1)
-  }
+// // Type Method
+// TypeWriter.prototype.type = function(){
+//   // Current Index of word
+//   const current = this.wordIndex % this.words.length
+//   // Get full text of current word
+//   const fullTXT = this.words[current]
 
-  // Insert TXT into Element
-  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+//   // Check if word is being deleted
+//   if(this.isDeleting){
+//     // Remove a character
+//     this.txt = fullTXT.substring(0, this.txt.length - 1)
+//   }else{
+//     // Adding a character
+//     this.txt = fullTXT.substring(0, this.txt.length + 1)
+//   }
 
-  // Initial Type Speed
-  let typeSpeed = 300
+//   // Insert TXT into Element
+//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
 
-  if(this.isDeleting){
-    typeSpeed /= 2
-  }
+//   // Initial Type Speed
+//   let typeSpeed = 300
 
-  // If word is complete
-  if(!this.isDeleting && this.txt === fullTXT){
-    // Cause a pause at the end
-    typeSpeed = this.wait
-    // Set delete to true
-    this.isDeleting = true
-  } else if(this.isDeleting && this.txt === ''){
+//   if(this.isDeleting){
+//     typeSpeed /= 2
+//   }
+
+//   // If word is complete
+//   if(!this.isDeleting && this.txt === fullTXT){
+//     // Cause a pause at the end
+//     typeSpeed = this.wait
+//     // Set delete to true
+//     this.isDeleting = true
+//   } else if(this.isDeleting && this.txt === ''){
+//     this.isDeleting = false
+//     // Move to next word
+//     this.wordIndex++
+//     // Pause before start typing
+//     typeSpeed = 500
+//   }
+
+//   setTimeout(()=> {this.type()}, typeSpeed)
+// }
+
+
+
+
+// ------ ES6 Class Method ------
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement
+    this.words = words
+    this.txt = ''
+    this.wordIndex = 0
+    this.wait = parseInt(wait, 10)
+    this.type()
     this.isDeleting = false
-    // Move to next word
-    this.wordIndex++
-    // Pause before start typing
-    typeSpeed = 500
   }
 
-  setTimeout(()=> {this.type()}, typeSpeed)
+  type() {
+    // Current Index of word
+    const current = this.wordIndex % this.words.length
+    // Get full text of current word
+    const fullTXT = this.words[current]
+
+    // Check if word is being deleted
+    if (this.isDeleting) {
+      // Remove a character
+      this.txt = fullTXT.substring(0, this.txt.length - 1)
+    } else {
+      // Adding a character
+      this.txt = fullTXT.substring(0, this.txt.length + 1)
+    }
+
+    // Insert TXT into Element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+    // Initial Type Speed
+    let typeSpeed = 300
+
+    if (this.isDeleting) {
+      typeSpeed /= 2
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTXT) {
+      // Cause a pause at the end
+      typeSpeed = this.wait
+      // Set delete to true
+      this.isDeleting = true
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false
+      // Move to next word
+      this.wordIndex++
+      // Pause before start typing
+      typeSpeed = 500
+    }
+
+    setTimeout(() => { this.type() }, typeSpeed)
+  }
 }
 
 // Init on DOM Load
@@ -61,7 +120,4 @@ function init(){
   const wait = txtElement.getAttribute('data-wait')
   new TypeWriter(txtElement, words, wait)
 }
-
-
-// ES6 Method
 
